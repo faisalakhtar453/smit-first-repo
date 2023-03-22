dc0a46644564bcfb77a2ba63c14d79121e4b859f
Faisal Akhtar <faisalakhtar453@gmail.com>
2023-03-22 13:00:52 +0500

    checkCity updated

--- js/script.js
+++ js/script.js
@@ -119,8 +119,25 @@ function checkCity() {
       },
       onClick: function(){} // Callback after click
     }).showToast();
+  }else{
+    document.getElementById("output").innerHTML =""
+    Toastify({
+      text: "Your added city is not in list",
+      duration: 3000,
+      destination: "https://github.com/apvarun/toastify-js",
+      newWindow: true,
+      close: true,
+      gravity: "top", // `top` or `bottom`
+      position: "right", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "linear-gradient(to right, #00b09b, #96c93d)",
+      },
+      onClick: function(){} // Callback after click
+    }).showToast();
+    
   }
-}
+  }
 
 
 console.log(cities);
